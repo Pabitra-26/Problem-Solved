d19f974869d8a8805a34b9fde5bd42acc900ede8
zugate <kumar.shubham@redbus.com>
2020-06-11 19:05:46 +0530

    Append and delete problem

---
+++ Hackerrank/AppendDelete.swift
@@ -0,0 +1,29 @@
+// Problem Name : Append and Delete
+//Description : 
+
+
+import Foundation
+
+func appendAndDelete(s: String, t: String, k: Int) -> String {
+
+    if s.count+t.count < k {
+        return("Yes")
+    }
+
+    var sArr = Array(s)
+    var tArr = Array(t)
+    var c = 0
+    let minA = min(s.count, t.count)
+    let maxA = max(s.count, t.count)
+
+    for i in 0..<minA{
+        if sArr[i] != tArr[i]{
+        break
+        }
+    c += 1
+    }
+    var stepNeeded = s.count + t.count - c*2;
+    return ((stepNeeded <= k && stepNeeded%2 == k%2) || s.count + t.count < k ? "Yes" : "No")
+}
+
+appendAndDelete(s:"hackerrank",t:"hackerhappy",k:9)
