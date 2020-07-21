71064cc093e8bd18eed7e6d0e79e75f6039f061a
Pabitra-26 <63382182+Pabitra-26@users.noreply.github.com>
2020-07-21 13:37:09 +0530

    Update Tree_Inorder_Traversal.py

--- Hackerrank/Tree_Inorder_Traversal.py
+++ Hackerrank/Tree_Inorder_Traversal.py
@@ -1,7 +1,7 @@
 # Problem name: Tree: Inorder Traversal
 # Description: Complete the inOrder function in your editor below, which has 1 parameter: a pointer to the root of a binary tree.
 # It must print the values in the tree's inorder traversal as a single line of space-separated values.
-# Strategy:
+# Strategy: use recursive method
 
 
 class Node:
