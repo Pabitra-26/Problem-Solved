16f125f416214b0eb33be60038991ec963d4b040
Pabitra-26 <63382182+Pabitra-26@users.noreply.github.com>
2020-07-21 13:38:02 +0530

    Update Tree_Postorder_Traversal.py

--- Hackerrank/Tree_Postorder_Traversal.py
+++ Hackerrank/Tree_Postorder_Traversal.py
@@ -1,7 +1,7 @@
 # Problem name: Tree: Postorder Traversal
 # Description: Complete the postOrder function in your editor below, which has 1 parameter: a pointer to the root of a binary tree.
 # It must print the values in the tree's postorder traversal as a single line of space-separated values.
-# Strategy:
+# Strategy: use recursive method
 
 class Node:
     def __init__(self, info):
