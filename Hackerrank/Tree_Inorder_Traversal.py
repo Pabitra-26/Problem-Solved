0eebf00e4d87abc8d3b7ed84a00cf2d541a7dece
Pabitra-26 <63382182+Pabitra-26@users.noreply.github.com>
2020-07-21 13:09:03 +0530

    Add files via upload

---
+++ Hackerrank/Tree_Inorder_Traversal.py
@@ -0,0 +1,60 @@
+# Problem name: Tree: Inorder Traversal
+# Description: Complete the inOrder function in your editor below, which has 1 parameter: a pointer to the root of a binary tree.
+# It must print the values in the tree's inorder traversal as a single line of space-separated values.
+# Strategy:
+
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+def inOrder(root):
+    if not root:
+        return
+    inOrder(root.left)
+    print(root.info, end=" ")
+    inOrder(root.right)
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+inOrder(tree.root)
---
+++ Hackerrank/Tree_Preorder_Traversal.py
@@ -0,0 +1,66 @@
+# Problem name: Tree: Preorder Traversal
+# Description: Complete the preOrder function in your editor below, which has 1 parameter: a pointer to the root of a binary tree.
+# It must print the values in the tree's preorder traversal as a single line of space-separated values.
+# Strategy: Use recursive method
+
+
+
+
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+
+def preOrder(root):
+    if not root:
+        return
+    print(root.info, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+    # Write your code here
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+preOrder(tree.root)
